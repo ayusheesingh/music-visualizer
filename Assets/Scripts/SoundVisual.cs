201e13a28ed6912e9eff009e48a239c8e9579f00
asingh <asingh@marqeta.com>
2023-05-16 11:49:33 -0700

    adding music visualizer files

---
+++ Assets/Scripts/SoundVisual.cs
@@ -0,0 +1,195 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundVisual : MonoBehaviour
+{
+    private const int SAMPLE_SIZE = 1024;
+
+    public float rmsValue;
+    public float dbValue;
+    public float pitchValue;
+    public bool keyPressed;
+
+    public float maxVisualScale = 25.0f;
+    public float visualModifier = 50.0f;
+    public float smoothSpeed = 10.0f;
+    public float keepPercentage = 0.25f;
+    public float threshold = 0.02f;      // minimum amplitude to extract pitch
+
+    public Material backgroundMaterial;
+    public Color minColor;
+    public Color maxColor;
+    private Color currentColor;
+
+    private AudioSource source;
+    private float[] samples;
+    private float[] spectrum;
+    private float sampleRate;
+
+    private Transform[] visualList;
+    private float[] visualScale;
+    private int amnVisual = 64;
+
+    private void Start() {
+        source = GetComponent<AudioSource>();
+        samples = new float[SAMPLE_SIZE];
+        spectrum = new float[SAMPLE_SIZE];
+        sampleRate = AudioSettings.outputSampleRate;
+        
+        SetBackground();
+        // SpawnLine();
+        SpawnCircle();
+    }
+
+    // This function creates a line of cubes rather than a circle 
+    private void SpawnLine() {
+        visualScale = new float[amnVisual];
+        visualList = new Transform[amnVisual];
+
+        // Creates amnVisual cubes right next to each other 
+        for (int i = 0; i < amnVisual; i++) {
+            // Can change the primitive type for a different "look"
+            GameObject go = GameObject.CreatePrimitive(PrimitiveType.Sphere) as GameObject;
+            visualList[i] = go.transform;
+            visualList[i].position = Vector3.right * i;
+        }
+    }
+
+    // Similar to SpawnLine, but creates a circle of cubes 
+    private void SpawnCircle() {
+        visualScale = new float[amnVisual];
+        visualList = new Transform[amnVisual]; 
+
+        Vector3 center = Vector3.zero;
+        float radius = 5.0f;
+
+        for (int i = 0; i < amnVisual; i++) {
+            float ang = i * 1.0f / amnVisual;
+            ang = ang * Mathf.PI * 2;
+
+            float x = center.x + Mathf.Cos(ang) * radius;
+            float y = center.y + Mathf.Sin(ang) * radius;
+
+            Vector3 pos = center + new Vector3(x, y, 0);
+            GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube) as GameObject;
+            go.transform.position = pos;
+            go.transform.rotation = Quaternion.LookRotation(Vector3.forward, pos);
+            visualList[i] = go.transform;
+        }
+    }
+
+    // Called every second 
+    private void Update() {
+        // if (Input.GetKeyDown(KeyCode.Space)) {
+        //     keyPressed = !keyPressed;
+        //     SpawnLine();
+        // } else {
+        //     SpawnCircle();
+        // }
+        // Music values updated every second 
+        AnalyzeSound();
+        // Visual is updated every second 
+        UpdateVisual();
+        SetBackground();
+    }
+
+    // Implement this however we want -based off of pitch and db
+    private void SetBackground()
+    {
+        Camera.main.backgroundColor = Color.white;
+    }
+
+    private void UpdateVisual() {
+        int spectrumIndex = 0;
+        int averageSize = (int)((SAMPLE_SIZE * keepPercentage) / amnVisual);
+
+        for (int visualIndex = 0; visualIndex < amnVisual; visualIndex++) {
+            int j = 0;
+            float sum = 0;
+            while (j < averageSize) {
+                sum += spectrum[spectrumIndex];
+                spectrumIndex++;
+                j++;
+            }
+            float scaleY = sum / averageSize * visualModifier;
+            visualScale[visualIndex] -= Time.deltaTime * smoothSpeed;
+            if (visualScale[visualIndex] < scaleY) {
+                visualScale[visualIndex] = scaleY;
+            }
+
+            if (visualScale[visualIndex] > maxVisualScale) {
+                visualScale[visualIndex] = maxVisualScale;
+            }
+
+            visualList[visualIndex].localScale = Vector3.one + Vector3.up * visualScale[visualIndex];
+            visualList[visualIndex].GetComponent<Renderer>().material.color = currentColor;            
+        }
+    }
+
+    // This code analyzes the input mp3 using digital signal processing -- from youtube video as well 
+    private void AnalyzeSound() {
+        source.GetOutputData(samples, 0);
+
+        // get rms value
+        int i = 0;
+        float sum = 0;
+        for (; i < SAMPLE_SIZE; i++) {
+            sum += samples[i] * samples[i];
+        }
+        rmsValue = Mathf.Sqrt(sum / SAMPLE_SIZE);
+
+        // get db value
+        dbValue = 20 * Mathf.Log10(rmsValue / 0.1f);
+        
+        if (dbValue < - 160) dbValue = -160; // clipping the value 
+
+        // get sound spectrum
+        source.GetSpectrumData(spectrum, 0, FFTWindow.BlackmanHarris);
+
+        // find pitch
+        float maxV = 0;
+        var maxN = 0;
+        for (i = 0; i < SAMPLE_SIZE; i++) {
+            if (spectrum[i] > maxV && spectrum[i] > threshold) 
+            {
+                maxV = spectrum[i];
+                maxN = i;
+            }         
+        }
+
+        float freqN = maxN;
+        if (maxN > 0 && maxN < SAMPLE_SIZE - 1) {
+            var dL = spectrum[maxN - 1] / spectrum[maxN];
+            var dR = spectrum[maxN + 1] / spectrum[maxN];
+            freqN += 0.5f * (dR * dR - dL * dL);
+        }
+        pitchValue = freqN * (sampleRate / 2) / SAMPLE_SIZE;
+
+        // Based on pitch, modify the current color 
+        // Added by us based on the values of the input song pitch
+        currentColor = Color.white;
+        if (pitchValue <= 1600) {
+            currentColor = Color.grey;
+        }
+        if (pitchValue <= 1200) {
+            currentColor = Color.red;
+        }
+        if (pitchValue <= 800) {
+            currentColor = Color.blue;
+        }
+        if (pitchValue <= 600) {
+            currentColor = Color.cyan;
+        }
+        if (pitchValue <= 400) {
+            currentColor = Color.green;
+        }
+        if (pitchValue <= 250) {
+            currentColor = Color.magenta;
+        }
+        if (pitchValue <= 50) {
+            currentColor = Color.black;
+        }
+
+    }
+}
---
+++ Assets/Scripts/VoronoiDiagram.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+ 
+using csDelaunay;
+ 
+// This class was written by the library creator, we modified it to fit our desired results 
+public class VoronoiDiagram : MonoBehaviour {
+ 
+    // The number of polygons/sites we want
+    public int polygonNumber = 200;
+ 
+    // This is where we will store the resulting data
+    public Dictionary<Vector2f, Site> sites;
+    private List<Edge> edges;
+    Voronoi voronoi;
+    int count;
+    
+    void Start() {
+        // Scale the quad 
+        Vector3 temp = transform.localScale;
+        temp.x = 25.0f;
+        temp.y = 25.0f;
+        transform.localScale = temp;
+
+        // Modify the position of the model
+        Vector3 pos = transform.localPosition;
+        pos.z = 3;
+        transform.localPosition = pos;
+
+        // Create your sites (lets call that the center of your polygons)
+        List<Vector2f> points = CreateRandomPoint();
+       
+        // Create the bounds of the voronoi diagram
+        // Use Rectf instead of Rect; it's a struct just like Rect and does pretty much the same,
+        // but like that it allows you to run the delaunay library outside of unity (which mean also in another tread)
+        Rectf bounds = new Rectf(0,0,512,512);
+       
+        // There is a two ways you can create the voronoi diagram: with or without the lloyd relaxation
+        // Here I used it with 2 iterations of the lloyd relaxation
+        voronoi = new Voronoi(points,bounds);
+ 
+        // Now retreive the edges from it, and the new sites position if you used lloyd relaxtion
+        sites = voronoi.SitesIndexedByLocation;
+        edges = voronoi.Edges;
+ 
+        DisplayVoronoiDiagram();
+    }
+   
+   // Created to generate a Voronoi animation with Lloyd relaxation applied 
+    void Update() {
+        voronoi.LloydRelaxation(count);
+        sites = voronoi.SitesIndexedByLocation;
+        edges = voronoi.Edges;
+ 
+        DisplayVoronoiDiagram();
+
+        count++;
+        count = count%10;
+
+        if (count == 1) {
+            List<Vector2f> points = CreateRandomPoint();
+            Rectf bounds = new Rectf(0,0,512,512);
+            voronoi = new Voronoi(points,bounds);
+        }
+    }
+
+    private List<Vector2f> CreateRandomPoint() {
+        // Use Vector2f, instead of Vector2
+        // Vector2f is pretty much the same than Vector2, but like you could run Voronoi in another thread
+        List<Vector2f> points = new List<Vector2f>();
+        for (int i = 0; i < polygonNumber; i++) {
+            points.Add(new Vector2f(Random.Range(0,512), Random.Range(0,512)));
+        }
+ 
+        return points;
+    }
+ 
+    // Here is a very simple way to display the result using a simple bresenham line algorithm
+    // Just attach this script to a quad
+    private void DisplayVoronoiDiagram() {
+        Texture2D tx = new Texture2D(512,512);
+        foreach (KeyValuePair<Vector2f,Site> kv in sites) {
+            tx.SetPixel((int)kv.Key.x, (int)kv.Key.y, Color.black);
+        }
+        foreach (Edge edge in edges) {
+            // if the edge doesn't have clippedEnds, if was not within the bounds, dont draw it
+            if (edge.ClippedEnds == null) continue;
+            Color random = new Color(
+                Random.Range(0f, 1f), 
+                Random.Range(0f, 1f), 
+                Random.Range(0f, 1f)
+            );
+            DrawLine(edge.ClippedEnds[LR.LEFT], edge.ClippedEnds[LR.RIGHT], tx, random);
+        }
+        tx.Apply();
+ 
+        this.GetComponent<Renderer>().material.mainTexture = tx;
+    }
+ 
+    // Bresenham line algorithm
+    private void DrawLine(Vector2f p0, Vector2f p1, Texture2D tx, Color c, int offset = 0) {
+        int x0 = (int)p0.x;
+        int y0 = (int)p0.y;
+        int x1 = (int)p1.x;
+        int y1 = (int)p1.y;
+       
+        int dx = Mathf.Abs(x1-x0);
+        int dy = Mathf.Abs(y1-y0);
+        int sx = x0 < x1 ? 1 : -1;
+        int sy = y0 < y1 ? 1 : -1;
+        int err = dx-dy;
+       
+        while (true) {
+            tx.SetPixel(x0+offset,y0+offset,c);
+           
+            if (x0 == x1 && y0 == y1) break;
+            int e2 = 2*err;
+            if (e2 > -dy) {
+                err -= dy;
+                x0 += sx;
+            }
+            if (e2 < dx) {
+                err += dx;
+                y0 += sy;
+            }
+        }
+    }
+}      
